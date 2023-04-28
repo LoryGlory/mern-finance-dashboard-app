4f89c552ae27d82276e3887fedda965dc4695a3b
Laura Roganovic <laura.roganovic@googlemail.com>
2023-04-28 14:34:48 +0200

    add theme import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import {useMemo} from "react";
+import {createTheme} from "@mui/material/styles";
+import {themeSettings} from "@/theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+
 function App() {
+    // load theme once on initial load
+    const theme = useMemo(() => createTheme(themeSettings), []);
+
     return (
         <div className='app'>
-
+            {/* theme settings */}
+            <ThemeProvider theme={theme}>
+                {/* reset css settings to default */}
+                <CssBaseline/>
+            </ThemeProvider>
         </div>
     )
 }
