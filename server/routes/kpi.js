1f4a6ad6a30b7460c0cf3955fc6d58986e3b4e18
Laura Roganovic <87700499+LoryGlory@users.noreply.github.com>
2023-05-02 21:21:30 +0200

    fix typo in catch block

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -8,7 +8,7 @@ router.get('/kpis', async (req, res) => {
   try {
     const kpis = await KPI.find();
     res.status(200).json({kpis});
-  } catch (err) {
+  } catch (error) {
     res.status(404).json({message: error.message});
   }
 });
