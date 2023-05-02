357f1a832c9903224db709450f9429eb90731588
Laura Roganovic <laura.roganovic@googlemail.com>
2023-05-02 12:14:42 +0200

    change color palettes and background color

--- src/theme.ts
+++ src/theme.ts
@@ -1,106 +1,108 @@
 export const tokens = {
-    grey: {
-        100: '#e1e3e6',
-        200: '#c2c7cd',
-        300: '#9ca2ac',
-        400: '#767f8b',
-        500: '#5c6370',
-        600: '#484f59',
-        700: '#3c4049',
-        800: '#33353c',
-        900: '#26282c',
-    },
-    primary: {
-        // blue/petrol
-        100: '#d6f1f7',
-        200: '#b2e2ef',
-        300: '#7dcce3',
-        400: '#41adcf',
-        500: '#2691b4',
-        600: '#247ba0',
-        700: '#22607c',
-        800: '#244f66',
-        900: '#224357',
-    },
-    secondary: {
-        // gold
-        100: '#fff8c5',
-        200: '#fff285',
-        300: '#ffe446',
-        400: '#ffd31b',
-        500: '#f5ab00',
-        600: '#e28800',
-        700: '#bb5f02',
-        800: '#984908',
-        900: '#7c3c0b',
-    },
-    tertiary: {
-        // indigo
-        500: "#8B85C1",
-    },
-    background: {
-        light: "#D7F2FE",
-        main: "#022B3C",
-    },
+  grey: {
+    100: '#f3f6f9',
+    200: '#d0dde7',
+    300: '#a7c0d2',
+    400: '#789eb8',
+    500: '#5782a0',
+    600: '#446985',
+    700: '#38556c',
+    800: '#31485b',
+    900: '#2d3e4d',
+    950: '#1e2933',
+  },
+  primary: {
+    // blue/petrol
+    100: '#e4eefa',
+    200: '#c2ddf5',
+    300: '#8cc3ed',
+    400: '#4fa3e1',
+    500: '#2988ce',
+    600: '#1a6baf',
+    700: '#16558e',
+    800: '#164976',
+    900: '#183e62',
+    950: '#0a1929',
+  },
+  secondary: {
+    // gold
+    100: '#fff8c5',
+    200: '#fff285',
+    300: '#ffe446',
+    400: '#ffd31b',
+    500: '#f5ab00',
+    600: '#e28800',
+    700: '#bb5f02',
+    800: '#984908',
+    900: '#7c3c0b',
+  },
+  tertiary: {
+    // indigo
+    500: '#8B85C1',
+  },
+  background: {
+    light: '#001e3c',
+    main: '#0a1929',
+  },
 };
 
 // mui theme settings
 export const themeSettings = {
-    palette: {
-        primary: {
-            ...tokens.primary,
-            main: tokens.primary[500],
-            light: tokens.primary[400],
-        },
-        secondary: {
-            ...tokens.secondary,
-            main: tokens.secondary[500],
-        },
-        tertiary: {
-            ...tokens.tertiary,
-        },
-        grey: {
-            ...tokens.grey,
-            main: tokens.grey[500],
-        },
-        background: {
-            default: tokens.background.main,
-            light: tokens.background.light,
-        },
+  palette: {
+    primary: {
+      ...tokens.primary,
+      main: tokens.primary[500],
+      light: tokens.primary[400],
     },
-    typography: {
-        fontFamily: ["Inter", "sans-serif"].join(","),
-        fontSize: 12,
-        h1: {
-            fontFamily: ["Inter", "sans-serif"].join(","),
-            fontSize: 32,
-        },
-        h2: {
-            fontFamily: ["Inter", "sans-serif"].join(","),
-            fontSize: 24,
-        },
-        h3: {
-            fontFamily: ["Inter", "sans-serif"].join(","),
-            fontSize: 20,
-            fontWeight: 800,
-            color: tokens.grey[200],
-        },
-        h4: {
-            fontFamily: ["Inter", "sans-serif"].join(","),
-            fontSize: 14,
-            fontWeight: 600,
-            color: tokens.grey[300],
-        },
-        h5: {
-            fontFamily: ["Inter", "sans-serif"].join(","),
-            fontSize: 12,
-            fontWeight: 400,
-            color: tokens.grey[500],
-        },
-        h6: {
-            fontFamily: ["Inter", "sans-serif"].join(","),
-            fontSize: 10,
-            color: tokens.grey[700],
-        },
+    secondary: {
+      ...tokens.secondary,
+      main: tokens.secondary[500],
     },
+    tertiary: {
+      ...tokens.tertiary,
+    },
+    grey: {
+      ...tokens.grey,
+      main: tokens.grey[500],
+    },
+    background: {
+      default: tokens.background.main,
+      light: tokens.background.light,
+    },
+  },
+  typography: {
+    fontFamily: ['Inter', 'sans-serif'].join(','),
+    fontSize: 12,
+    h1: {
+      fontFamily: ['Inter', 'sans-serif'].join(','),
+      fontSize: 32,
+    },
+    h2: {
+      fontFamily: ['Inter', 'sans-serif'].join(','),
+      fontSize: 24,
+    },
+    h3: {
+      fontFamily: ['Inter', 'sans-serif'].join(','),
+      fontSize: 20,
+      fontWeight: 800,
+      color: tokens.grey[200],
+    },
+    h4: {
+      fontFamily: ['Inter', 'sans-serif'].join(','),
+      fontSize: 14,
+      fontWeight: 600,
+      color: tokens.grey[300],
+    },
+    h5: {
+      fontFamily: ['Inter', 'sans-serif'].join(','),
+      fontSize: 12,
+      fontWeight: 400,
+      color: tokens.grey[500],
+    },
+    h6: {
+      fontFamily: ['Inter', 'sans-serif'].join(','),
+      fontSize: 10,
+      color: tokens.grey[700],
+    },
+  },
 };
