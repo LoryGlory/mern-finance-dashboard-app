463096b16c076e5712c8b3153390aad0e90be5da
Laura Roganovic <87700499+LoryGlory@users.noreply.github.com>
2023-05-02 15:05:48 +0200

    add routes for KPIs

---
+++ server/routes/kpi.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/kpis', async (req, res) => {
+  // grab data from database and catch error
+  try {
+    const kpis = await KPI.find();
+  } catch (err) {
+    res.status(404).json({message: error.message});
+  }
+});
+
+export default router;
