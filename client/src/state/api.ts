4bb72975598fb051019a7d267bc48a8b4e40457e
Laura Roganovic <87700499+LoryGlory@users.noreply.github.com>
2023-05-03 13:26:36 +0200

    add products to redux toolkit query and types importg

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,16 +1,20 @@
 // react redux query component for api calls
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import {GetKpisResponse} from './types';
+import {GetKpisResponse, GetProductsResponse} from './types';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}), reducerPath: 'main',
-  tagTypes: ['Kpis'],
+  tagTypes: ['Kpis', 'Products'], // information is saved here
   endpoints: (build) => ({
     getKpis: build.query<Array<GetKpisResponse>, void>({
       query: () => 'kpi/kpis/',
       providesTags: ['Kpis'],
     }),
+    getProducts: build.query<Array<GetProductsResponse>, void>({
+      query: () => 'product/products/',
+      providesTags: ['Products'],
+    }),
   }),
 });
 
-export const {useGetKpisQuery} = api;
+export const {useGetKpisQuery, useGetProductsQuery} = api;
