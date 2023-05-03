3364cf90b67cd96dad4ad894068f0da9ab36f425
Laura Roganovic <87700499+LoryGlory@users.noreply.github.com>
2023-05-03 10:55:50 +0200

    add GetKpisResponse import from /types

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,9 @@
 // react redux query component for api calls
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import {GetKpisResponse} from '@/state/types';
+import {GetKpisResponse} from './types';
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
-  reducerPath: 'main',
+  baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}), reducerPath: 'main',
   tagTypes: ['Kpis'],
   endpoints: (build) => ({
     getKpis: build.query<Array<GetKpisResponse>, void>({
