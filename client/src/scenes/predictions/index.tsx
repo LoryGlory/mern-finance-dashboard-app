c3c62b736a55b6735d5d6e8a2a497267cb483e82
Laura Roganovic <87700499+LoryGlory@users.noreply.github.com>
2023-05-03 22:00:54 +0200

    initial commit for prediction route scene

---
+++ client/src/scenes/predictions/index.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {Box, Typography, useTheme} from '@mui/material';
+import {useGetKpisQuery} from '@/state/api';
+import DashboardBox from '@/components/DashboardBox';
+import FlexBetween from '@/components/FlexBetween';
+
+type Props = {}
+
+const Predictions = (props: Props) => {
+  const {palette} = useTheme();
+  const {isPredictions, setIsPredictions} = useState(false);
+  const {data: kpiData} = useGetKpisQuery();
+
+
+  return (
+    <DashboardBox width='100%' height='100%' p='1rem' overflow='hidden'>
+      <FlexBetween m='1rem 2.5rem'>
+        <Box>
+          <Typography variant='h3'>
+            Revenue and Predictions
+          </Typography>
+          <Typography variant='h6'>
+            charted revenue and predicted revenue based on a simple linear regression model
+          </Typography>
+        </Box>
+      </FlexBetween>
+    </DashboardBox>
+  );
+};
+
+export default Predictions;
