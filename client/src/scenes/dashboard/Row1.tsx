6c6374506ad4365c23891b9e1558be9dda274e0f
Laura Roganovic <87700499+LoryGlory@users.noreply.github.com>
2023-05-03 12:41:11 +0200

    add third DashboardBox and useMemo hook

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -3,7 +3,7 @@ import DashboardBox from '@/components/DashboardBox';
 import {useGetKpisQuery} from '@/state/api';
 import {
   Area,
-  AreaChart,
+  AreaChart, Bar, BarChart,
   CartesianGrid,
   Legend,
   Line,
@@ -21,7 +21,7 @@ const Row1 = () => {
   const {data} = useGetKpisQuery();
   console.log('Data', data);
 
-  // useMemo hook for first DashboardBox data
+  // formatting + useMemo hook for first DashboardBox data
   const revenueExpenses = useMemo(() => {
     return (
       data &&
@@ -35,7 +35,7 @@ const Row1 = () => {
     );
   }, [data]);
 
-  // useMemo hook for second DashboardBox data
+  // formatting + useMemo hook for second DashboardBox data
   const revenueProfit = useMemo(() => {
     return (
       data &&
@@ -44,6 +44,19 @@ const Row1 = () => {
           name: month.substring(0, 3),
           revenue: revenue,
           profit: (revenue - expenses).toFixed(2),
+        };
+      })
+    );
+  }, [data]);
+
+  // formatting + useMemo hook for third DashboardBox data
+  const revenue = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({month, revenue}) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
         };
       })
     );
@@ -180,7 +193,55 @@ const Row1 = () => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea='c'></DashboardBox>
+      <DashboardBox gridArea='c'>
+        <BoxHeader
+          title='Revenue Month by Month'
+          subtitle='graph representing the revenue month by month'
+          sideText='+4%'
+        />
+        <ResponsiveContainer width='100%' height='100%'>
+          <BarChart
+            width={500}
+            height={300}
+            data={revenue}
+            margin={{
+              top: 17,
+              right: 15,
+              left: -5,
+              bottom: 58,
+            }}
+          >
+            <defs>
+              <linearGradient id='colorRevenue' x1='0' y1='0' x2='0' y2='1'>
+                <stop
+                  offset='5%'
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset='95%'
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey='name'
+              axisLine={false}
+              tickLine={false}
+              style={{fontSize: '10px'}}
+            />
+            <YAxis
+              axisLine={false}
+              tickLine={false}
+              style={{fontSize: '10px'}}
+            />
+            <Tooltip />
+            <Bar dataKey='revenue' fill='url(#colorRevenue)' />
+          </BarChart>
+        </ResponsiveContainer>
+      </DashboardBox>
     </>
   );
 };
